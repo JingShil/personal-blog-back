6bb38ac3572eeac553e934e636e633f8ecb7ad49
JingShil <1444894773@qq.com>
2024-03-26 23:46:51 +0800

    3 commit

--- src/test/java/com/ccsu/blog/personal_blog/PersonalBlogApplicationTests.java
+++ src/test/java/com/ccsu/blog/personal_blog/PersonalBlogApplicationTests.java
@@ -1,13 +1,13 @@
 package com.ccsu.blog.personal_blog;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PersonalBlogApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
